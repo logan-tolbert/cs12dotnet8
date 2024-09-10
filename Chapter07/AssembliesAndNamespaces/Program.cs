22af92b00a36f8438f7087b973ab0cb929e1e408
logan-tolbert <logo88205+git@gmail.com>
2024-09-10 18:21:16 -0500

    Add code examples

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;// to use XDocument
+using System; // To use String
+
+XDocument doc = new();
+
+String s1 = "Hello";
+String s2 = "World";
+
+WriteLine($"{s1} {s2}");
+
+#region Understanding native=sized integers
+
+WriteLine($"Environment.Is64BitProcess = {Environment.Is64BitProcess}");
+WriteLine($"int.MaxValue = {int.MaxValue}");
+WriteLine($"nint.MaxValue = {nint.MaxValue}");
+
+#endregion
